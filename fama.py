1a6223d630dbaef042fdd0863223ee26d904bea4
SageNBD <vitoraug96@gmail.com>
2020-08-13 12:23:09 -0300

    Returns and Sentiment added for each day

--- fama.py
+++ fama.py
@@ -3,7 +3,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 import pandas as pd
 import datetime
 import yfinance as yf
-import numpy as np
 
 def get_collections(db, assets):
     asset_collection = {} # Create collection for each asset
@@ -26,6 +25,32 @@ def get_historical_data(assets):
                 end='2019-12-31')['Adj Close'])
     return hist_data
 
+def populate_dictionary(sia):
+    print('Reading data from Excel sheet...')
+    df_dictionary = pd.read_excel('dictionary.xlsx').filter(
+                                ['Word', 'Negative_Unity', 'Positive_Unity'])
+    new_words = {}
+    print('Creating dictionary...', end=' ')
+    for index, row in df_dictionary.iterrows():
+        if row['Negative_Unity'] or row['Positive_Unity']:
+            neg = row['Negative_Unity']
+            pos = row['Positive_Unity']
+            
+            value = pos if pos != 0 else -neg
+            
+            new_words[row['Word'].lower()] = value
+    print('DONE')
+    sia.lexicon.update(new_words)
+    
+    petr = {
+        'moro': -1.60591526778577,
+        'lava': -1.60591526778577,
+        'jato': -1.60591526778577,
+        'STF': -1.60591526778577      
+    }
+    sia.lexicon.update(petr)
+
+    
 # MongoDB Setup
 client = MongoClient('localhost', 27017)
 assert('fama' in client.list_database_names())
@@ -33,6 +58,7 @@ db = client.fama
 
 # Sentiment Intensity Analyser
 sia = SIA()
+populate_dictionary(sia)
 
 assets = ['itau', 'ambev', 'petrobras']
 
@@ -40,8 +66,7 @@ asset_collection = get_collections(db, assets)
 hist_data = get_historical_data(assets)
 
 scores_df = {}
-
-for asset in assets:
+for asset in assets: # FUNCAO
     start = datetime.datetime(2016, 1, 1)
     end = datetime.datetime(2019, 12, 31)
     
@@ -60,14 +85,35 @@ for asset in assets:
                 + sia.polarity_scores(doc['body'])['compound']
             )
         
-        score.append(pol_score)
+        num_docs = docs.count()
+        score.append(pol_score / num_docs if num_docs else .0)
+        
         curr += datetime.timedelta(days=1)
     
-    scores_df[asset] = pd.DataFrame({'Pol Score': score}, index=pd.date_range(start=start, end=end))
+    scores_df[asset] = pd.DataFrame({'Pol Score': score}, 
+                                    index=pd.date_range(start=start, end=end)
+                                    )
     print('DONE')
 
-dataframes = {}
+df = {}
+for asset in assets: # FUNCAO
+    df[asset] = scores_df[asset].join(hist_data[asset])
+    #df[asset].bfill(inplace=True)
+    df[asset].dropna(inplace=True)
+    df[asset]['Returns'] = (
+        df[asset]['Adj Close'] / df[asset]['Adj Close'].shift(1) - 1
+    )
+    df[asset]['Pred Score'] = df[asset]['Pol Score'].shift(1)
+
+df_pred = {}
+df_corr = {}
 for asset in assets:
-    #dataframes[asset] = hist_data[asset].join(scores_df[asset])
-    dataframes[asset] = scores_df[asset].join(hist_data[asset])
-    dataframes[asset].bfill(inplace=True)
+    df_pred[asset] = df[asset].filter(['Pred Score', 'Returns'])
+    df_corr[asset] = df_pred[asset].corr()
+    
+df_pred_final = {}
+df_corr2 = {}
+for asset in assets:
+    df_pred_final[asset] = df_pred[asset][(df_pred[asset]['Pred Score'] >= 0.5) |
+                                (df_pred[asset]['Pred Score'] <= -0.5)]
+    df_corr2[asset] = df_pred_final[asset].corr()
