7f03c40815c2f9ba04237845ef8953b2ecd93052
SageNBD <vitoraug96@gmail.com>
2020-08-13 22:05:56 -0300

    Added stuff

--- fama.py
+++ fama.py
@@ -3,6 +3,9 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 import pandas as pd
 import datetime
 import yfinance as yf
+from pytrends.request import TrendReq
+
+new_words = {}
 
 def get_collections(db, assets):
     asset_collection = {} # Create collection for each asset
@@ -11,6 +14,16 @@ def get_collections(db, assets):
 
     return asset_collection
 
+def setup_db(assets):
+    # MongoDB Setup
+    client = MongoClient('localhost', 27017)
+    assert('fama' in client.list_database_names())
+    db = client.fama
+    
+    asset_collection = get_collections(db, assets)
+    
+    return db, asset_collection
+    
 def get_historical_data(assets):
     hist_data = {}
     ticker = {
@@ -25,11 +38,20 @@ def get_historical_data(assets):
                 end='2019-12-31')['Adj Close'])
     return hist_data
 
+def get_trends(assets):
+    pytrends = TrendReq(hl='pt-BR')
+    pytrends.build_payload(kw_list=assets, 
+                            timeframe='2016-01-01 2019-12-31',
+                            cat=7)
+    df_trends = pytrends.interest_over_time()
+    
+    return df_trends
+
 def populate_dictionary(sia):
     print('Reading data from Excel sheet...')
     df_dictionary = pd.read_excel('dictionary.xlsx').filter(
                                 ['Word', 'Negative_Unity', 'Positive_Unity'])
-    new_words = {}
+    #new_words = {}
     print('Creating dictionary...', end=' ')
     for index, row in df_dictionary.iterrows():
         if row['Negative_Unity'] or row['Positive_Unity']:
@@ -50,70 +72,98 @@ def populate_dictionary(sia):
     }
     sia.lexicon.update(petr)
 
-    
-# MongoDB Setup
-client = MongoClient('localhost', 27017)
-assert('fama' in client.list_database_names())
-db = client.fama
+def setup_sia():
+    # Sentiment Intensity Analyser
+    sia = SIA()
+    populate_dictionary(sia)
+    return sia
 
-# Sentiment Intensity Analyser
-sia = SIA()
-populate_dictionary(sia)
+def calc_polarity_scores(assets, asset_collection, sia):
+    scores_df = {}
+    
+    for asset in assets:
+        start = datetime.datetime(2016, 1, 1)
+        end = datetime.datetime(2019, 12, 31)
+        
+        score = []
+        curr = start
+        print(f'Getting pol scores for {asset}...', end=' ')
+        while curr <= end:
+            docs = asset_collection[asset].find(
+                {"date": curr.strftime("%d/%m/%y")}
+            )
+            
+            pol_score = 0
+            for doc in docs:
+                pol_score += (
+                    sia.polarity_scores(doc['headline'])['compound']
+                    + sia.polarity_scores(doc['body'])['compound']
+                )
+            
+            #num_docs = docs.count()
+            #score.append(pol_score / num_docs if num_docs else .0)
+            score.append(pol_score)
+            
+            curr += datetime.timedelta(days=1)
+        
+        scores_df[asset] = pd.DataFrame(
+                        {'Pol Score': score}, 
+                        index=pd.date_range(start=start, end=end)
+        )
+        print('DONE')  
+    
+    return scores_df
+
+def merge_dataframes(scores_df, hist_data, trend_df):
+    df = {}
+    for asset in assets: # Merge each dataframe
+        tmp = {}
+        # Join each dataframe
+        tmp = scores_df[asset].join(hist_data[asset]).join(
+                                        trend_df.filter([asset]))
+        # Fill each day with associated week trend value
+        tmp[asset].ffill(inplace=True)
+        # Dismiss days with no trading
+        tmp.dropna(subset=['Adj Close'], inplace=True)
+        
+        # Create returns column 
+        tmp['Returns'] = (
+            tmp['Adj Close'] / tmp['Adj Close'].shift(1) - 1
+        )
+        # Shift polarity scores down to predict prices
+        tmp['Pred Score'] = tmp['Pol Score'].shift(1)
+        df[asset] = tmp
+        
+    return df
 
 assets = ['itau', 'ambev', 'petrobras']
+db, asset_collection = setup_db(assets)
+sia = setup_sia()
 
-asset_collection = get_collections(db, assets)
+scores_df = calc_polarity_scores(assets, asset_collection, sia)
 hist_data = get_historical_data(assets)
+trend_df = get_trends(assets)
 
-scores_df = {}
-for asset in assets: # FUNCAO
-    start = datetime.datetime(2016, 1, 1)
-    end = datetime.datetime(2019, 12, 31)
-    
-    score = []
-    curr = start
-    print(f'Getting pol scores for {asset}...', end=' ')
-    while curr <= end:
-        docs = asset_collection[asset].find(
-            {"date": curr.strftime("%d/%m/%y")}
-        )
-        
-        pol_score = 0
-        for doc in docs:
-            pol_score += (
-                sia.polarity_scores(doc['headline'])['compound']
-                + sia.polarity_scores(doc['body'])['compound']
-            )
-        
-        num_docs = docs.count()
-        score.append(pol_score / num_docs if num_docs else .0)
-        
-        curr += datetime.timedelta(days=1)
-    
-    scores_df[asset] = pd.DataFrame({'Pol Score': score}, 
-                                    index=pd.date_range(start=start, end=end)
-                                    )
-    print('DONE')
+df = merge_dataframes(scores_df, hist_data, trend_df)
 
-df = {}
-for asset in assets: # FUNCAO
-    df[asset] = scores_df[asset].join(hist_data[asset])
-    #df[asset].bfill(inplace=True)
-    df[asset].dropna(inplace=True)
-    df[asset]['Returns'] = (
-        df[asset]['Adj Close'] / df[asset]['Adj Close'].shift(1) - 1
-    )
-    df[asset]['Pred Score'] = df[asset]['Pol Score'].shift(1)
+df_pred_sent = {}
+df_corr_sent = {}
 
-df_pred = {}
-df_corr = {}
+df_pred_trend = {}
+df_corr_trend = {}
 for asset in assets:
-    df_pred[asset] = df[asset].filter(['Pred Score', 'Returns'])
-    df_corr[asset] = df_pred[asset].corr()
-    
-df_pred_final = {}
-df_corr2 = {}
-for asset in assets:
-    df_pred_final[asset] = df_pred[asset][(df_pred[asset]['Pred Score'] >= 0.5) |
-                                (df_pred[asset]['Pred Score'] <= -0.5)]
-    df_corr2[asset] = df_pred_final[asset].corr()
+    # Sentiment
+    df_pred_sent[asset] = df[asset].filter(['Pred Score', 'Returns']) 
+    df_pred_sent[asset].plot(x="Pred Score", y="Returns", style='o', title=asset)  
+    df_corr_sent[asset] = df_pred_sent[asset].corr()
+    # Trends
+    df_pred_trend[asset] = df[asset].filter(['Returns', asset]) 
+    df_pred_trend[asset].plot(x=asset, y="Returns", style='o', title=asset)  
+    df_corr_trend[asset] = df_pred_sent[asset].corr()
+ 
+# df_pred_final = {}
+# df_corr2 = {}
+# for asset in assets:
+#     df_pred_final[asset] = df_pred[asset][(df_pred[asset]['Pred Score'] >= 0.5) |
+#                                 (df_pred[asset]['Pred Score'] <= -0.5)]
+#     df_corr2[asset] = df_pred_final[asset].corr()
