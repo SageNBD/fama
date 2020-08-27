81e5924d3461bff063eb9da3a7705443327edf05
SageNBD <vitoraug96@gmail.com>
2020-08-27 15:21:36 -0300

    Changed a few dataframes

--- fama.py
+++ fama.py
@@ -5,8 +5,6 @@ import datetime
 import yfinance as yf
 from pytrends.request import TrendReq
 
-new_words = {}
-
 def get_collections(db, assets):
     asset_collection = {} # Create collection for each asset
     for asset in assets:
@@ -52,7 +50,7 @@ def populate_dictionary(sia):
     print('Reading data from Excel sheet...')
     df_dictionary = pd.read_excel('sheets/dictionary.xlsx').filter(
                                 ['Word', 'Negative_Unity', 'Positive_Unity'])
-    #new_words = {}
+    new_words = {}
     print('Creating dictionary...', end=' ')
     for index, row in df_dictionary.iterrows():
         if row['Negative_Unity'] or row['Positive_Unity']:
@@ -64,14 +62,6 @@ def populate_dictionary(sia):
             new_words[row['Word'].lower()] = value
     print('DONE')
     sia.lexicon.update(new_words)
-    
-    petr = {
-        'moro': -1.60591526778577,
-        'lava': -1.60591526778577,
-        'jato': -1.60591526778577,
-        'STF': -1.60591526778577      
-    }
-    sia.lexicon.update(petr)
 
 def setup_sia():
     # Sentiment Intensity Analyser
@@ -137,39 +127,17 @@ def merge_dataframes(scores_df, hist_data, trend_df):
         
     return df
 
-assets = ['itau', 'ambev', 'petrobras']
-db, asset_collection = setup_db(assets)
-sia = setup_sia()
+if __name__ == "__main__":
+    assets = ['itau', 'ambev', 'petrobras']
+    db, asset_collection = setup_db(assets)
+    sia = setup_sia()
 
-scores_df = calc_polarity_scores(assets, asset_collection, sia)
-hist_data = get_historical_data(assets)
-trend_df = get_trends(assets)
+    scores_df = calc_polarity_scores(assets, asset_collection, sia)
+    hist_data = get_historical_data(assets)
+    trend_df = get_trends(assets)
 
-df = merge_dataframes(scores_df, hist_data, trend_df)
+    df = merge_dataframes(scores_df, hist_data, trend_df)
 
-for asset in assets:
-    df[asset].to_csv(f'dados_{asset}.csv')
-    df[asset].to_excel(f'dados_{asset}.xlsx', engine='xlsxwriter')
-
-
-df_pred_sent = {}
-df_corr_sent = {}
-
-df_pred_trend = {}
-df_corr_trend = {}
-for asset in assets:
-    # Sentiment
-    df_pred_sent[asset] = df[asset].filter(['Pred Score', 'Returns']) 
-    df_pred_sent[asset].plot(x="Pred Score", y="Returns", style='o', title=asset)  
-    df_corr_sent[asset] = df_pred_sent[asset].corr()
-    # Trends
-    df_pred_trend[asset] = df[asset].filter(['Returns', asset]) 
-    df_pred_trend[asset].plot(x=asset, y="Returns", style='o', title=asset)  
-    df_corr_trend[asset] = df_pred_sent[asset].corr()
- 
-df_pred_final = {}
-df_corr2 = {}
-for asset in assets:
-    df_pred_final[asset] = df_pred_sent[asset][(df_pred_sent[asset]['Pred Score'] >= 0.2) |
-                                (df_pred_sent[asset]['Pred Score'] <= -0.2)]
-    df_corr2[asset] = df_pred_final[asset].corr()
+    for asset in assets:
+        df[asset].to_csv(f'dados_{asset}.csv')
+        df[asset].to_excel(f'dados_{asset}.xlsx', engine='xlsxwriter')
